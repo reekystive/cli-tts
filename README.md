d76d5d076fb16f6119a7f326684619cc4fb910ea
ReekyStive <reekystive@gmail.com>
2021-06-25 22:28:25 +0800

    Add multi-language support

--- README.md
+++ README.md
@@ -1,3 +1,60 @@
 # cli-tts
 
 Command Line Text-To-Speech using Google TTS
+
+This is an interactive command line text-to-speech tool. Just enter text and the voice will be played.
+
+Multi-language is supported.
+
+## Quick Start
+
+### macOS
+
+``` bash
+$ git clone https://github.com/ReekyStive/cli-tts.git
+$ cd cli-tts
+
+$ brew install ffmpeg
+# be sure ffmpeg is in PATH
+$ pip install -r requirements.txt
+
+$ python tts.py
+```
+
+### Windows
+
+There's a package that already included everything:
+[Win32 Release](https://github.com/) // TODO
+
+## Screenshots
+
+// TODO
+
+## Supported Languages
+
+| Local accent              | Language code | Top-level domain |
+| ------------------------- | ------------- | ---------------- |
+| English (Australia)       | `en`          | `com.au`         |
+| English (United Kingdom)  | `en`          | `co.uk`          |
+| English (United States)   | `en`          | `com`            |
+| English (Canada)          | `en`          | `ca`             |
+| English (India)           | `en`          | `co.in`          |
+| English (Ireland)         | `en`          | `ie`             |
+| English (South Africa)    | `en`          | `co.za`          |
+| French (Canada)           | `fr`          | `ca`             |
+| French (France)           | `fr`          | `fr`             |
+| Mandarin (China Mainland) | `zh-CN`       | `<any>`          |
+| Mandarin (Taiwan)         | `zh-TW`       | `<any>`          |
+| Portuguese (Brazil)       | `pt`          | `com.br`         |
+| Portuguese (Portugal)     | `pt`          | `pt`             |
+| Spanish (Mexico)          | `es`          | `com.mx`         |
+| Spanish (Spain)           | `es`          | `es`             |
+| Spanish (United States)   | `es`          | `com`            |
+
+## Requirements
+
+- Python 3.x
+- Pip packages
+  - gTTS
+  - simpleaudio
+- ffmpeg (in PATH)
--- tts.py
+++ tts.py
@@ -5,20 +5,64 @@ import os
 import sys
 
 temp = './temp/'
+langs_help = """
+| Local accent              | Language code | Top-level domain |
+| ------------------------- | ------------- | ---------------- |
+| English (Australia)       | en            | com.au           |
+| English (United Kingdom)  | en            | co.uk            |
+| English (United States)   | en            | com              |
+| English (Canada)          | en            | ca               |
+| English (India)           | en            | co.in            |
+| English (Ireland)         | en            | ie               |
+| English (South Africa)    | en            | co.za            |
+| French (Canada)           | fr            | ca               |
+| French (France)           | fr            | fr               |
+| Mandarin (China Mainland) | zh-CN         | any              |
+| Mandarin (Taiwan)         | zh-TW         | any              |
+| Portuguese (Brazil)       | pt            | com.br           |
+| Portuguese (Portugal)     | pt            | pt               |
+| Spanish (Mexico)          | es            | com.mx           |
+| Spanish (Spain)           | es            | es               |
+| Spanish (United States)   | es            | com              |
+"""
 
-print('Google TTS version 1.0 by ReekyStive')
-print('Type ":quit" to quit, type ":proxy" to set proxy.')
+print('Google TTS version 1.1 by ReekyStive')
+print('Type ":quit" to quit, type ":help" for help.')
 print('Type anything to start.')
+
+lang = 'en'
+tld = 'com'
 
 while (True):
     s = input('> ')
     if s.strip() == ':quit':
         break
     elif s.strip() == ':proxy':
-        server = input('HTTP Proxy server: ')
+        server = input('HTTP proxy server: ')
         os.environ['http_proxy'] = server
         os.environ['https_proxy'] = server
         print('Proxy is set.\n')
+        continue
+    elif s.strip() == ':help':
+        print(':quit  - quit')
+        print(':help  - show help')
+        print(':proxy - set proxy')
+        print(':lang  - set language and local accent')
+        print(':langs - show all languages and local accents')
+        print()
+        continue
+    elif s.strip() == ':lang':
+        lang = input('Language code(default: en): ')
+        if lang.strip() == '':
+            lang = 'en'
+        tld = input('Top-level domain(default: com): ')
+        if tld.strip() == '':
+            tld = 'com'
+        print('Language and local accent is set.')
+        print()
+        continue
+    elif s.strip() == ':langs':
+        print(langs_help)
         continue
 
     print('Generating...', end='')
@@ -28,7 +72,7 @@ while (True):
     except:
         pass
     try:
-        tts = gtts(s)
+        tts = gtts(s, lang=lang, tld=tld)
         tts.save(temp + 'speech.mp3')
     except AssertionError:
         print(' Nothing to generate.\n')
